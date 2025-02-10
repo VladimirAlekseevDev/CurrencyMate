1ac3ba1f3221aac54b1e77caa177dbf8d8fba84c
Vladimir Alekseev <vladimir.alekseev.dev@gmail.com>
2025-02-10 15:18:36 +0400

    Base project structure and dependencies

---
+++ CurrencyMate/src/test/java/dev/sgd/currencymate/ApplicationStartTest.java
@@ -0,0 +1,13 @@
+package dev.sgd.currencymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStartTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dev.sgd/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+O
+src/main/java/dev/sgd/Main.java,b\d\bd351b1572df0dd505be3e835cecfefd6db0d330
