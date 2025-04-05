a3b88cc905dfd70d582008fc6fd67607d65da585
Vladimir Alekseev <vladimir.alekseev.dev@gmail.com>
2025-04-05 17:57:11 +0400

    Added logging

--- src/main/java/dev/sgd/currencymate/Application.java
+++ src/main/java/dev/sgd/currencymate/Application.java
@@ -13,7 +13,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        log.info("Application Default Time Zone: {}", TimeZone.getDefault().getID());
+        log.info("⌚ Currency Mate Application Default Time Zone: {}", TimeZone.getDefault().getID());
     }
 
 }
