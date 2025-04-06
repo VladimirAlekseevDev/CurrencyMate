dfd0ec1ea43488ca4df446e4c908bfe18c7453ab
Vladimir Alekseev <vladimir.alekseev.dev@gmail.com>
2025-04-06 13:58:00 +0400

    Swagger annotations added to DTOs

--- modules/rest-api/src/main/java/dev/sgd/currencymate/rest/api/model/error/ErrorResponse.java
+++ modules/rest-api/src/main/java/dev/sgd/currencymate/rest/api/model/error/ErrorResponse.java
@@ -1,14 +1,18 @@
 package dev.sgd.currencymate.rest.api.model.error;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Error response DTO")
 public class ErrorResponse {
 
+    @Schema(description = "Error human readable message")
     private String message;
 
+    @Schema(description = "Error text code")
     private String code;
 
 }
--- modules/rest-api/src/main/java/dev/sgd/currencymate/rest/api/model/response/ExchangeRateResponse.java
+++ modules/rest-api/src/main/java/dev/sgd/currencymate/rest/api/model/response/ExchangeRateResponse.java
@@ -1,6 +1,7 @@
 package dev.sgd.currencymate.rest.api.model.response;
 
 import dev.sgd.currencymate.rest.api.model.CurrencyDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,18 +12,25 @@ import java.time.OffsetDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Exchange rate information between two currencies")
 public class ExchangeRateResponse {
 
+    @Schema(description = "From currency details")
     private CurrencyDto from;
 
+    @Schema(description = "To currency details")
     private CurrencyDto to;
 
+    @Schema(description = "Name of the currency exchange data provider")
     private String providerName;
 
+    @Schema(description = "Exchange rate value")
     private BigDecimal rate;
 
+    @Schema(description = "Timestamp when data was last refreshed by provider")
     private OffsetDateTime lastRefreshed;
 
+    @Schema(description = "Timestamp when data was received by Currency Mate application")
     private OffsetDateTime receivedAt;
 
 }
